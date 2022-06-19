6ad4844aa4278ccb1a321fb4edf922a777e1ed05
Lucas Pontes <75232236+Bridges369@users.noreply.github.com>
2022-06-18 22:44:02 -0300

    map

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,40 @@ const MAP_HEIGHT: i32 = 45;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
- r: 50,
- g: 50,
- b: 150,
+    r: 50,
+    g: 50,
+    b: 150,
 };
 
-const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+const LIMIT_FPS: i32 = 60; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
+/// A tile of the map and its properties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
 
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
 
 /// This is a generic object: the player, a monster, an item, the stairs...
 /// It's always represented by a character on screen.
@@ -39,12 +60,14 @@ struct Object {
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
         Object { x, y, char, color }
- }
+    }
 
     /// move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     /// set the color and then draw the character that represents this object at its position
@@ -54,11 +77,56 @@ impl Object {
     }
 }
 
+type Map = Vec<Vec<Tile>>;
+struct Game {
+    map: Map,
+}
 
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
- // todo: handle keys
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    // draw all objects in the list
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+
+    // blit the contents of "con" to the root console and present it
+    blit(
+        &tcod.con, // <- from
+        (0, 0),
+        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        &mut tcod.root, // <- to
+        (0, 0),
+        1.0, // foreground transparency
+        1.0, // background transparency
+    );
+}
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+    // todo: handle keys
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -76,17 +144,16 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
         Key { code: Escape, .. } => return true, // exit game
 
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
 
         _ => {}
     }
 
- false
+    false
 }
-
 
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
@@ -103,20 +170,18 @@ fn main() {
     // update Tcod struct
     let mut tcod = Tcod { root, con };
 
-    let mut player_x = MIDDLE_WW;
-    let mut player_y = MIDDLE_WH;
-
-
-
     // create object representing the player
     let player = Object::new(MIDDLE_WW, MIDDLE_WH, '@', WHITE);
-
     // create an NPC
     let npc = Object::new(MIDDLE_WW - 5, MIDDLE_WH, '@', YELLOW);
 
     // the list of objects with those two
     let mut objects = [player, npc];
 
+    let game = Game {
+        // generate map (at this point it's not drawn to the screen)
+        map: make_map(),
+    };
 
     while !tcod.root.window_closed() {
         // clear the screen of the previous frame
@@ -126,28 +191,16 @@ fn main() {
         for object in &objects {
             object.draw(&mut tcod.con);
         }
-
-
-
-
-        // blit the contents of "con" to the root console and present it
-        blit(
-            &tcod.con, // <- from
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root, // <- to
-            (0, 0),
-            1.0, // foreground transparency
-            1.0, // background transparency
-        );
-
+        // render the screen
+        render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
         // handle keys and exit game if needed
 
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, &game, player);
         if exit {
+            println!("godbye");
             break;
         }
     }
